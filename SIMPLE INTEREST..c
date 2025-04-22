f22a84e6a506db0b8ee30520ed358b2b73b87f74
Doreen-irungu <doreenirungu429@gmail.com>
2025-04-22 05:51:49 +0300

    Update SIMPLE INTEREST..c

--- SIMPLE INTEREST..c
+++ SIMPLE INTEREST..c
@@ -7,12 +7,17 @@ Date:16/02/2025
 
 #include <iostream>
 #include <cmath>
+
 using namespace std;
+
 float addition(int x, int y){
+  
 int sum;	sum=x+y;
 return sum;}
+
 double simple_interest(float principle, float rate, float time){
 float interest;interest = principle*rate*time;return interest;}
+
 int main() {
 int sum;
 float interest;
@@ -20,4 +25,5 @@ sum = addition (5 , 10);
 interest=simple_interest(1000.0,4.0,0.2);
 cout<< "Sum: " <<sum<<endl;
 cout<< "Simple Interest: " <<interest<<endl;
+  
 return 0;}
